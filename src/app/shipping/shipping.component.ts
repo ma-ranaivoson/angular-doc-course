9b04f7973a4ba215cdbc7a1b45023eefada31845
ma-ranaivoson <rv.nirmalala@gmail.com>
2022-09-14 15:44:55 +0300

    Addiong Shipping

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts!: Observable<{ type: string; price: number }[]>;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
-
 }
