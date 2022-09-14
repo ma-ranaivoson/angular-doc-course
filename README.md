96371d566bd75a151ddf8aa00b4c785d28c2962c
ma-ranaivoson <rv.nirmalala@gmail.com>
2022-09-14 15:24:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sjkgzn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sjkgzn)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(desc: string) {
+    window.alert(desc);
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
